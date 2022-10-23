9b08a696fb1ee3253cb1c17e07f578445ee5303f
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-23 23:07:43 +0300

    initial commit

---
+++ urlshortener/shortener/views.py
@@ -0,0 +1,27 @@
+import uuid
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Url
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'shortener/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+
+        return HttpResponse(uid)
+
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+
+    return redirect('https://'+url_details.link)
